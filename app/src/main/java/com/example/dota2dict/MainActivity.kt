28ba745974c87be693deaf689afe56ae3fca1d16
johnaeris <barrotjohnaeris@gmail.com>
2022-09-25 11:59:46 +0800

    added top bar

--- app/src/main/java/com/example/dota2dict/MainActivity.kt
+++ app/src/main/java/com/example/dota2dict/MainActivity.kt
@@ -3,81 +3,62 @@ package com.example.dota2dict
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.dota2dict.ui.theme.Dota2DictTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            HomePage()
+
         }
     }
 }
 
 
 
-
 @Composable
-fun HomePage() {
-    Text(text = "Hello World")
-}
+fun TopBar() {
+    TopAppBar (
+        backgroundColor = colorResource(R.color.black),
+        contentColor = colorResource(R.color.white))
 
-@Preview(name = "Home Page", showBackground = true, showSystemUi = true)
-@Composable
-fun HomePagePreview() {
-    Dota2DictTheme {
-        HomePage()
+    {
+        Image(
+            painter = painterResource(R.drawable.dota2_logo),
+            contentDescription = "Logo",
+            modifier = Modifier
+                .size(70.dp)
+                .padding(start = 20.dp, top = 20.dp))
+
+        Spacer(modifier = Modifier.width(5.dp))
+
+        Text(
+            text = stringResource(R.string.app_name),
+            style = MaterialTheme.typography.h1,
+            modifier = Modifier
+                .background(colorResource(R.color.black))
+                .padding(start = 20.dp, top = 20.dp))
     }
 }
 
 
-
+@Preview(name = "Top Bar", showBackground = true)
 @Composable
-fun HeroesPage() {
-
-}
-
-@Preview(name = "Heroes Page", showBackground = true, showSystemUi = true)
-@Composable
-fun HeroesPagePreview() {
+fun TopBarPreview() {
     Dota2DictTheme {
-        HeroesPage()
-    }
-}
-
-
-
-@Composable
-fun ItemsPage() {
-
-}
-
-@Preview(name = "Items Page", showBackground = true, showSystemUi = true)
-@Composable
-fun ItemsPagePreview() {
-    Dota2DictTheme {
-        ItemsPage()
-    }
-}
-
-
-
-@Composable
-fun SettingsPage() {
-
-}
-
-@Preview(name = "Settings Page", showBackground = true, showSystemUi = true)
-@Composable
-fun SettingsPagePreview() {
-    Dota2DictTheme {
-        SettingsPage()
+        TopBar()
     }
 }
