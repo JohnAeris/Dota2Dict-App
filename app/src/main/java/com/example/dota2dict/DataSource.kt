a38f879e7542cd437c8f0b34653b4c058c0f561b
johnaeris <barrotjohnaeris@gmail.com>
2022-09-27 10:33:17 +0800

    renamed file itemsSource -> DataSource

---
+++ app/src/main/java/com/example/dota2dict/DataSource.kt
@@ -0,0 +1,27 @@
+package com.example.dota2dict
+
+class GeneralUpdateDataSource() {
+    fun loadGeneralUpdates(): List<GeneralUpdatesData> {
+        return listOf<GeneralUpdatesData>(
+            GeneralUpdatesData(R.string.text1),
+            GeneralUpdatesData(R.string.text2),
+            GeneralUpdatesData(R.string.text3),
+            GeneralUpdatesData(R.string.text4),
+            GeneralUpdatesData(R.string.text5),
+            GeneralUpdatesData(R.string.text6),
+            GeneralUpdatesData(R.string.text7),
+            GeneralUpdatesData(R.string.text8),
+            GeneralUpdatesData(R.string.text9),
+            GeneralUpdatesData(R.string.text10),
+            GeneralUpdatesData(R.string.text11),
+            GeneralUpdatesData(R.string.text12),
+            GeneralUpdatesData(R.string.text13),
+            GeneralUpdatesData(R.string.text14),
+            GeneralUpdatesData(R.string.text15),
+            GeneralUpdatesData(R.string.text16),
+            GeneralUpdatesData(R.string.text17),
+            GeneralUpdatesData(R.string.text18),
+            GeneralUpdatesData(R.string.text19)
+        )
+    }
+}
