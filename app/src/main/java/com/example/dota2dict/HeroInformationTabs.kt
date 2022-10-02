777899e900bfde8ef882ed34b960cce330b6811e
johnaeris <barrotjohnaeris@gmail.com>
2022-10-02 21:31:41 +0800

    created HeroInformationTabs.kt file for tabs

---
+++ app/src/main/java/com/example/dota2dict/HeroInformationTabs.kt
@@ -0,0 +1,74 @@
+package com.example.dota2dict
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import com.google.accompanist.pager.ExperimentalPagerApi
+import com.google.accompanist.pager.HorizontalPager
+import com.google.accompanist.pager.PagerState
+import com.google.accompanist.pager.rememberPagerState
+
+@OptIn(ExperimentalPagerApi::class)
+@Composable
+fun HeroTabLayout() {
+    val pagerState= rememberPagerState(pageCount = 4)
+    
+    Column(
+        modifier = Modifier.background(Color.Black))
+    {
+
+        HeroTabContent(pagerState = pagerState)
+
+    }
+}
+
+@Composable
+@Preview
+fun HeroTabLayoutPrev() {
+    HeroTabLayout()
+}
+
+@OptIn(ExperimentalPagerApi::class)
+@Composable
+fun HeroTabContent(pagerState: PagerState) {
+
+    HorizontalPager(state = pagerState) {
+        page -> when (page) {
+            0 -> HeroTabScreen(data = "Page 1")
+        
+            1 -> HeroTabScreen(data = "Page 2")
+        
+            2 -> HeroTabScreen(data = "Page 3")
+        
+            3 -> HeroTabScreen(data = "Page 4")
+        }
+    }
+}
+
+@Composable
+fun HeroTabScreen(data: String) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center)
+    {
+        Text(
+            text = data,
+            color = Color.White,
+            fontSize = 30.sp,
+            textAlign = TextAlign.Center,
+            fontWeight = FontWeight.Bold)
+    }
+}
+
+
