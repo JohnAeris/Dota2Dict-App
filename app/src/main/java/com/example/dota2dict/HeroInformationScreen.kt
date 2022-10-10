d0944cba1fa4a8711c491322b469cf301c79ccf2
johnaeris <barrotjohnaeris@gmail.com>
2022-10-10 23:00:01 +0800

    change some code

--- app/src/main/java/com/example/dota2dict/HeroInformationScreen.kt
+++ app/src/main/java/com/example/dota2dict/HeroInformationScreen.kt
@@ -1,18 +1,24 @@
 package com.example.dota2dict
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.material.Divider
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.dota2dict.ui.theme.Poppins
 import com.google.accompanist.pager.ExperimentalPagerApi
 import com.google.accompanist.pager.HorizontalPager
 import com.google.accompanist.pager.PagerState
@@ -32,11 +38,11 @@ fun HeroTabScreen() {
     }
 }
 
-@Composable
+/*@Composable
 @Preview
 fun HeroTabLayoutPrev() {
     HeroTabScreen()
-}
+}*/
 
 @OptIn(ExperimentalPagerApi::class)
 @Composable
@@ -44,19 +50,67 @@ fun HeroTabContent(pagerState: PagerState) {
 
     HorizontalPager(state = pagerState) {
             page -> when (page) {
-        0 -> HeroTabScreen(data = "Page 1")
+        0 -> HeroTabScreen1(data = "Page 1")
 
-        1 -> HeroTabScreen(data = "Page 2")
+        1 -> HeroTabScreen2(data = "Page 2")
 
-        2 -> HeroTabScreen(data = "Page 3")
+        2 -> HeroTabScreen3(data = "Page 3")
 
-        3 -> HeroTabScreen(data = "Page 4")
+        3 -> HeroTabScreen4(data = "Page 4")
     }
     }
 }
 
 @Composable
-fun HeroTabScreen(data: String) {
+fun HeroTabScreen1(data: String) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center)
+    {
+        Text(
+            text = data,
+            color = Color.White,
+            fontSize = 30.sp,
+            textAlign = TextAlign.Center,
+            fontWeight = FontWeight.Bold)
+    }
+}
+
+@Composable
+fun HeroTabScreen2(data: String) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center)
+    {
+        Text(
+            text = data,
+            color = Color.White,
+            fontSize = 30.sp,
+            textAlign = TextAlign.Center,
+            fontWeight = FontWeight.Bold)
+    }
+}
+
+@Composable
+fun HeroTabScreen3(data: String) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center)
+    {
+        Text(
+            text = data,
+            color = Color.White,
+            fontSize = 30.sp,
+            textAlign = TextAlign.Center,
+            fontWeight = FontWeight.Bold)
+    }
+}
+
+@Composable
+fun HeroTabScreen4(data: String) {
     Column(
         modifier = Modifier.fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally,
