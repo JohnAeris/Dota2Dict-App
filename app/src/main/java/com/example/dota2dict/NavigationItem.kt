953c473dccff1913e8d55f658bfd9bd1ce6c938d
johnaeris <barrotjohnaeris@gmail.com>
2022-10-13 00:10:01 +0800

    remove Setting Navigation

--- app/src/main/java/com/example/dota2dict/NavigationItem.kt
+++ app/src/main/java/com/example/dota2dict/NavigationItem.kt
@@ -4,7 +4,6 @@ sealed class NavigationItem(var route: String, var icon: Int, var title: String)
     object Home: NavigationItem("home", R.drawable.ic_home_page_icon, "Home")
     object Heroes: NavigationItem("heroes", R.drawable.ic_hero_page_icon, "Heroes")
     object Items: NavigationItem("items", R.drawable.ic_item_page_icon, "Items")
-    object Settings: NavigationItem("settings", R.drawable.ic_setting_page_icon, "Settings")
 }
 
 sealed class NavigationScreen(val route: String) {
